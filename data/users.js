acfa8cc727e4645ca63ddbd1034c0ba9a3c43887
Lucas Ferreira <ferreirak.lucas@gmail.com>
2021-04-24 14:29:05 -0400

    finished 'users' routes

--- data/users.js
+++ data/users.js
@@ -1,31 +1,32 @@
 const mongoCollections = require('./../config/mongoCollections');
 const ObjectId = require('mongodb').ObjectId;
 const users = mongoCollections.users;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-async function create(firstName, lastName, dateOfBirth, username, password, address, photoLink) {
+async function create(firstName, lastName, dateOfBirth, username, password, address, photoLink, email) {
     // handle inputs
-    try {
-        await checkInputs();
-    } catch (e) {
-        throw new Error(e);
+    const errorObj = await checkInputs(firstName, lastName, dateOfBirth, username, password, address, photoLink, email, 'POST');
+    if (errorObj.error) {
+        throw errorObj.message;
     }
-
-    firstName = firstName.trim();
-    lastName = lastName.trim();
-    username = username.trim();
-    password = password.trim();
-    photoLink = photoLink.trim();
 
     const usersCollection = await users();
 
     let newUser = {
-        firstName: firstName,
-        lastName: lastName,
-        dateOfBirth: dateOfBirth,
-        username: username,
-        password: password,
-        address: address,
-        photoLink: photoLink
+        firstName: firstName.trim(),
+        lastName: lastName.trim(),
+        dateOfBirth: new Date(dateOfBirth),
+        username: username.trim().toLowerCase(),
+        password: await bcrypt.hash(password.trim(), saltRounds),
+        address: {  "street": address.street.trim(),
+                    "aptNo": address.aptNo ? address.aptNo.trim() : '',
+                    "zipCode": address.zipCode.trim(),
+                    "state": address.state.trim().toUpperCase(),
+                    "town": address.town.trim(),
+                    "country": "USA"},
+        photoLink: photoLink.trim(),
+        email: email.trim().toLowerCase()
     };
 
     const insertInfo = await usersCollection.insertOne(newUser);
@@ -33,9 +34,9 @@ async function create(firstName, lastName, dateOfBirth, username, password, addr
 
     const newId = insertInfo.insertedId;
 
-    const book = await this.readByID(newId.toString());
+    const user = await this.readByID(newId.toString());
 
-    return book;
+    return user;
 }
 
 async function readByID(id) {
@@ -54,9 +55,308 @@ async function readByID(id) {
     return user;
 }
 
-// to be implemented
-async function checkInputs() {
+async function readByUsername(username) {
+    // handle inputs
+    if (username === undefined) throw 'Input must be provided for \'username\' parameter!';
+    if (typeof username !== 'string') throw 'Input username must be a string!';
 
+    const usersCollection = await users();
+
+    const user = await usersCollection.findOne({ username: username.toLowerCase() });
+    if (user === null) throw 'No user with that username!';
+    user._id = user._id.toString();
+    user.dateOfBirth = `${user.dateOfBirth.getMonth()+1}/${user.dateOfBirth.getDate()}/${user.dateOfBirth.getFullYear()}`;
+
+    return user;
 }
 
-module.exports = {create, readByID};
+async function remove(id) {
+    // handle inputs
+    if (id === undefined) throw 'Input must be provided!';
+    if (typeof id !== 'string') throw 'Input must be a string!';
+    if (!ObjectId.isValid(id)) throw 'Input must be a valid ObjectID!';
+
+    const usersCollection = await users();
+
+    const user = await this.readByID(id);
+    const deletionInfo = await usersCollection.deleteOne({ _id: ObjectId(id) });
+
+    if (deletionInfo.deletedCount === 0) {
+        throw `Could not delete user with id of ${id}!`;
+    }
+
+    return `${user.userName} has been successfully deleted`;
+}
+
+async function update(updateObj) {
+    if (typeof updateObj !== 'object' || updateObj === null) {
+        throw "Input for parameter 'address' must be an object";
+    }
+
+    let {id, firstName, lastName, dateOfBirth, password, address, photoLink, email} = updateObj;
+    let user = {};
+
+    // check if id is valid
+    if (id === undefined || typeof id !== 'string') throw "String input must be provided for 'id' parameter!";
+
+    if (!ObjectId.isValid(id)) {
+        throw 'Input id must be a valid ObjectID';
+    }
+
+    try {
+        user = await readByID(id);
+    } catch (e) {
+        throw new Error(e);
+    }
+
+    // handle inputs
+    if (firstName === undefined && lastName === undefined && dateOfBirth === undefined && password === undefined &&
+        address === undefined && photoLink === undefined && email === undefined) {
+
+        throw "Input must be provided for at least one of the following parameters: 'firstName', 'lastName', 'dateOfBirth'," +
+        " 'password', 'address', 'photoLink', 'email'.";
+    }
+
+    if (email !== undefined) {
+        const errorObj = await checkEmail(email);
+        if (errorObj.error) {
+            throw errorObj.message;
+        }
+    }
+
+    try {
+        await checkInputs(  firstName === undefined ? user.firstName : firstName,
+                            lastName === undefined ? user.lastName : lastName,
+                            dateOfBirth === undefined ? user.dateOfBirth : dateOfBirth,
+                            user.username,
+                            password === undefined ? user.password : password,
+                            address === undefined ? user.address : address,
+                            photoLink === undefined ? user.photoLink : photoLink,
+                            email === undefined ? user.email : email,
+                            'PATCH');
+    } catch (e) {
+        throw new Error(e);
+    }
+
+    const usersCollection = await users()
+
+    const updatedUser = {
+        firstName: firstName === undefined ? user.firstName : firstName.trim(),
+        lastName: lastName === undefined ? user.lastName : lastName.trim(),
+        dateOfBirth: dateOfBirth === undefined ? new Date(user.dateOfBirth) : new Date(dateOfBirth),
+        username: user.username,
+        password: password === undefined ? user.password : await bcrypt.hash(password.trim(), saltRounds),
+        address: address === undefined ? user.address : {  "street": address.street.trim(),
+                                                            "aptNo": address.aptNo ? address.aptNo.trim() : '',
+                                                            "zipCode": address.zipCode.trim(),
+                                                            "state": address.state.trim().toUpperCase(),
+                                                            "town": address.town.trim(),
+                                                            "country": "USA"},
+        photoLink: photoLink === undefined ? user.photoLink : photoLink.trim(),
+        email: email === undefined ? user.email : email.trim().toLowerCase()
+    };
+
+    const updatedInfo = await usersCollection.updateOne(
+        { _id: ObjectId(id) },
+        { $set: updatedUser }
+    );
+    if (updatedInfo.modifiedCount === 0) {
+        throw 'Could not update user successfully!';
+    }
+
+    return await this.readByID(id);
+}
+
+async function usernameExists(username) {
+    const usersCollection = await users();
+
+    const user = await usersCollection.findOne({ username: username.toLowerCase() });
+    return user !== null;
+}
+
+async function emailExists(email) {
+    const usersCollection = await users();
+
+    const user = await usersCollection.findOne({ email: email.toLowerCase() });
+    return user !== null;
+}
+
+async function checkString(str, paramName) {
+    let error = false;
+    let message = '';
+
+    if (typeof str !== 'string') {
+        error = true;
+        message = `Input for parameter '${paramName}' must be a non-empty string`;
+        return {error: error, message: message};
+    }
+    if (str === '') {
+        error = true;
+        message = `Input for parameter '${paramName}' must be a non-empty string`;
+        return {error: error, message: message};
+    }
+
+    return {error: error, message: message};
+}
+
+async function checkDateOfBirth(dateOfBirth, dateOfBirthObj) {
+    let error = false;
+    let message = '';
+
+    if (!dateOfBirthObj instanceof Date || isNaN(dateOfBirthObj.valueOf())) {
+        error = true;
+        message = "Input for parameter 'dateOfBirth' must be in a valid string date format";
+        return {error: error, message: message};
+    }
+    return {error: error, message: message};
+}
+
+async function checkAddress(address) {
+    let error = false;
+    let message = '';
+
+    if (typeof address !== 'object' || address === null) {
+        error = true;
+        message = "Input for parameter 'address' must be an object";
+        return {error: error, message: message};
+    }
+    if (!'street' in address || !'zipCode' in address || !'state' in address || !'town' in address
+        || typeof address.street !== 'string' || typeof address.zipCode !== 'string'
+        || typeof address.state !== 'string' || typeof address.town !== 'string') {
+
+        error = true;
+        message = "Object provided for parameter 'address' must contain keys 'street', 'zipCode', 'state', and 'town', " +
+            "whose values must be non-empty strings";
+        return {error: error, message: message};
+    }
+
+    // check aptNo
+    if ('aptNo' in address) {
+        if (typeof address.aptNo !== 'string') {
+            error = true;
+            message = "Parameter 'address.aptNo' must be a string";
+            return {error: error, message: message};
+        }
+    }
+
+    address.street = address.street.trim();
+    address.zipCode = address.zipCode.trim();
+    address.state = address.state.trim();
+    address.town = address.town.trim();
+
+    if (address.street === '' || address.zipCode === '' || address.state === '' || address.town === '') {
+        error = true;
+        message = "Object provided for parameter 'address' must contain keys 'street', 'zipCode', 'state', and 'town', " +
+            "whose values must be non-empty strings";
+        return {error: error, message: message};
+    }
+
+    // check for valid US zip code
+    if (!/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(address.zipCode)) {
+        error = true;
+        message = "'zipCode' parameter must a valid US zip code";
+        return {error: error, message: message};
+    }
+
+    const states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
+                    'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR',
+                    'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC']
+    if (!states.includes(address.state.toUpperCase())) {
+        error = 400;
+        message = "'state' parameter must a valid US state abbreviation of the format 'XX'";
+        return {error: error, message: message};
+    }
+
+    return {error: error, message: message};
+}
+
+async function checkEmail(email) {
+    let error = false;
+    let message = '';
+
+    // check if email is valid
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+    {
+        error = true;
+        message = "Email must be in a valid format";
+        return {error: error, message: message};
+    }
+
+    // check if email is in use
+    if (await emailExists(email)) {
+        error = true;
+        message = "Email is already in use";
+        return {error: error, message: message};
+    }
+
+    return {error: error, message: message};
+}
+
+async function checkInputs(firstName, lastName, dateOfBirth, username, password, address, photoLink, email, requestType) {
+    let error = false;
+    let message = '';
+    let errorObj = {}
+    const dateOfBirthObj = new Date(dateOfBirth);
+
+    if (firstName === undefined || lastName === undefined || dateOfBirth === undefined || username === undefined ||
+        password === undefined || address === undefined || photoLink === undefined || email === undefined) {
+        error = true;
+        message = "Inputs must be provided for the following parameters: 'firstName', 'lastName', 'dateOfBirth', 'username'," +
+            " 'password', 'address', 'photoLink', and 'email'.";
+        return {error: error, message: message};
+    }
+
+    // trim string inputs
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    dateOfBirth = dateOfBirth.trim();
+    username = username.trim();
+    password = password.trim();
+    photoLink = photoLink.trim();
+    email = email.trim();
+
+    for (const [inputName, input] of [  ["firstName", firstName], ["lastName", lastName], ["dateOfBirth", dateOfBirth], ["username", username],
+        ["password", password], ["photoLink", photoLink], ["email", email]]) {
+        errorObj = await checkString(input, inputName);
+        if (errorObj.error) {
+            return errorObj;
+        }
+    }
+
+    errorObj = await checkDateOfBirth(dateOfBirth, dateOfBirthObj);
+    if (errorObj.error) {
+        return errorObj;
+    }
+
+    if (requestType === 'POST') {
+        if (await usernameExists(username)) {
+            error = true;
+            message = "Username already exists";
+            return {error: error, message: message};
+        }
+    }
+
+    errorObj = await checkAddress(address);
+    if (errorObj.error) {
+        return errorObj;
+    }
+
+    // check photo link
+    if (!/^([a-z]:)?((\\|\/)[a-z0-9\s_@\-^!#$%&+={}\[\]]+)+\.(tif|tiff|bmp|jpg|jpeg|png|gif)$/i.test(photoLink)) {
+        error = true;
+        message = "'photoLink' parameter must be a valid full file path for an image of one the following types: .tif, .tiff, " +
+            ".bmp, 'jpg', 'jpeg', 'png', 'gif'.";
+        return {error: error, message: message};
+    }
+
+    if (requestType === 'POST') {
+        errorObj = await checkEmail(email);
+        if (errorObj.error) {
+            return errorObj;
+        }
+    }
+
+    return {error: error, message: ""};
+}
+
+module.exports = {create, readByID, readByUsername, remove, update, usernameExists, emailExists};
--- routes/users.js
+++ routes/users.js
@@ -5,27 +5,315 @@ const usersData = data.users;
 const ObjectId = require('mongodb').ObjectId;
 
 router.post('/', async (req, res) => {
-    let {firstName, lastName, dateOfBirth, username, password, address, photoLink} = req.body;
+    let {firstName, lastName, dateOfBirth, username, password, address, photoLink, email} = req.body;
 
-    // // handle inputs
-    const {errorCode, message} = await checkInputs();
+    // handle inputs
+    const {errorCode, message} = await checkInputs(firstName, lastName, dateOfBirth, username, password, address, photoLink, email, 'POST');
     if (errorCode !== 0) {
         res.status(errorCode).json({error: message});
         return;
     }
 
-    dateOfBirth = new Date(dateOfBirth);
-
     try {
-        const newUser = await usersData.create(firstName, lastName, dateOfBirth, username, password, address, photoLink);
+        const newUser = await usersData.create(firstName, lastName, dateOfBirth, username, password, address, photoLink, email);
         res.json(newUser);
     } catch (e) {
         res.sendStatus(500);
     }
 });
 
-// to be implemented
-async function checkInputs() {
+router.get('/:id', async (req, res) => {
+    // handle inputs
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json({error: 'Input id must be a valid ObjectID'});
+        return;
+    }
+
+    try {
+        let user = await usersData.readByID(req.params.id);
+        res.json(user);
+    } catch (e) {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+router.get('/username/:username', async (req, res) => {
+    try {
+        let user = await usersData.readByUsername(req.params.username);
+        res.json(user);
+    } catch (e) {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    // check if id is valid
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json({error: 'Input id must be a valid ObjectID'});
+        return;
+    }
+
+    // check if id exists
+    try {
+        await usersData.readByID(req.params.id);
+    } catch (e) {
+        res.status(404).json({error: 'User id could not be found'});
+        return;
+    }
+
+    try {
+        await usersData.remove(req.params.id);
+        res.json({userId: req.params.id, deleted: true});
+    } catch (e) {
+        res.sendStatus(500);
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    let {firstName, lastName, dateOfBirth, password, address, photoLink, email} = req.body;
+
+    let user = {};
+
+    // check if id is valid
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json({error: 'Input id must be a valid ObjectID'});
+        return;
+    }
+
+    // check if id exists
+    try {
+        user = await usersData.readByID(req.params.id);
+    } catch (e) {
+        res.status(400).json({error: 'User id could not be found'});
+        return;
+    }
+
+    // handle inputs
+    if (firstName === undefined && lastName === undefined && dateOfBirth === undefined && password === undefined &&
+        address === undefined && photoLink === undefined && email === undefined) {
+
+        res.status(400).json({
+            error: "Input must be provided for at least one of the following parameters: 'firstName', 'lastName', 'dateOfBirth'," +
+                " 'password', 'address', 'photoLink', 'email'."});
+        return;
+    }
+
+    if (email !== undefined) {
+        const errorObj = await checkEmail(email);
+        if (errorObj.errorCode !== 0) {
+            res.status(400).json({
+                error: errorObj.message});
+            return;
+        }
+    }
+
+    const {errorCode, message} = await checkInputs(firstName === undefined ? user.firstName : firstName,
+                                                    lastName === undefined ? user.lastName : lastName,
+                                                    dateOfBirth === undefined ? user.dateOfBirth : dateOfBirth,
+                                                    user.username,
+                                                    password === undefined ? user.password : password,
+                                                    address === undefined ? user.address : address,
+                                                    photoLink === undefined ? user.photoLink : photoLink,
+                                                    email === undefined ? user.email : email,
+                                                    'PATCH');
+    if (errorCode !== 0) {
+        res.status(errorCode).json({error: message});
+        return;
+    }
+
+    try {
+        const user = await usersData.update({"id": req.params.id,
+                                                "firstName": firstName,
+                                                "lastName": lastName,
+                                                "dateOfBirth": dateOfBirth,
+                                                "password": password,
+                                                "address": address,
+                                                "photoLink": photoLink,
+                                                "email": email
+        })
+        res.json(user);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+})
+
+async function checkString(str, paramName) {
+    let errorCode = 0;
+    let message = '';
+
+    if (typeof str !== 'string') {
+        errorCode = 400;
+        message = `Input for parameter '${paramName}' must be a non-empty string`;
+        return {errorCode: errorCode, message: message};
+    }
+    if (str === '') {
+        errorCode = 400;
+        message = `Input for parameter '${paramName}' must be a non-empty string`;
+        return {errorCode: errorCode, message: message};
+    }
+
+    return {errorCode: errorCode, message: message};
+}
+
+async function checkDateOfBirth(dateOfBirth, dateOfBirthObj) {
+    let errorCode = 0;
+    let message = '';
+
+    if (!dateOfBirthObj instanceof Date || isNaN(dateOfBirthObj.valueOf())) {
+        errorCode = 400;
+        message = "Input for parameter 'dateOfBirth' must be in a valid string date format";
+        return {errorCode: errorCode, message: message};
+    }
+    return {errorCode: errorCode, message: message};
+}
+
+async function checkAddress(address) {
+    let errorCode = 0;
+    let message = '';
+
+    if (typeof address !== 'object' || address === null) {
+        errorCode = 400;
+        message = "Input for parameter 'address' must be an object";
+        return {errorCode: errorCode, message: message};
+    }
+    if (!'street' in address || !'zipCode' in address || !'state' in address || !'town' in address
+        || typeof address.street !== 'string' || typeof address.zipCode !== 'string'
+        || typeof address.state !== 'string' || typeof address.town !== 'string') {
+
+        errorCode = 400;
+        message = "Object provided for parameter 'address' must contain keys 'street', 'zipCode', 'state', and 'town', " +
+            "whose values must be non-empty strings";
+        return {errorCode: errorCode, message: message};
+    }
+
+    // check aptNo
+    if ('aptNo' in address) {
+        if (typeof address.aptNo !== 'string') {
+            errorCode = 400;
+            message = "Parameter 'address.aptNo' must be a string";
+            return {errorCode: errorCode, message: message};
+        }
+    }
+
+    address.street = address.street.trim();
+    address.zipCode = address.zipCode.trim();
+    address.state = address.state.trim();
+    address.town = address.town.trim();
+
+    if (address.street === '' || address.zipCode === '' || address.state === '' || address.town === '') {
+        errorCode = 400;
+        message = "Object provided for parameter 'address' must contain keys 'street', 'zipCode', 'state', and 'town', " +
+            "whose values must be non-empty strings";
+        return {errorCode: errorCode, message: message};
+    }
+
+    // check for valid US zip code
+    if (!/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(address.zipCode)) {
+        errorCode = 400;
+        message = "'zipCode' parameter must a valid US zip code";
+        return {errorCode: errorCode, message: message};
+    }
+
+    const states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
+        'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR',
+        'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC']
+    if (!states.includes(address.state.toUpperCase())) {
+        errorCode = 400;
+        message = "'state' parameter must a valid US state abbreviation of the format 'XX'";
+        return {errorCode: errorCode, message: message};
+    }
+
+    return {errorCode: errorCode, message: message};
+}
+
+async function checkEmail(email) {
+    let errorCode = 0;
+    let message = '';
+
+    // check if email is valid
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+    {
+        errorCode = 400;
+        message = "Email must be in a valid format";
+        return {errorCode: errorCode, message: message};
+    }
+
+    // check if email is in use
+    if (await usersData.emailExists(email)) {
+        errorCode = 400;
+        message = "Email is already in use";
+        return {errorCode: errorCode, message: message};
+    }
+
+    return {errorCode: errorCode, message: message};
+}
+
+async function checkInputs(firstName, lastName, dateOfBirth, username, password, address, photoLink, email, requestType) {
+    let errorCode = 0;
+    let message = '';
+    let errorObj = {}
+    const dateOfBirthObj = new Date(dateOfBirth);
+
+    if (firstName === undefined || lastName === undefined || dateOfBirth === undefined || username === undefined ||
+        password === undefined || address === undefined || photoLink === undefined || email === undefined) {
+        errorCode = 400;
+        message = "Inputs must be provided for the following parameters: 'firstName', 'lastName', 'dateOfBirth', 'username'," +
+            " 'password', 'address', 'photoLink', and 'email'.";
+        return {errorCode: errorCode, message: message};
+    }
+
+    // trim string inputs
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    dateOfBirth = dateOfBirth.trim();
+    username = username.trim();
+    password = password.trim();
+    photoLink = photoLink.trim();
+    email = email.trim();
+
+    for (const [inputName, input] of [  ["firstName", firstName], ["lastName", lastName], ["dateOfBirth", dateOfBirth], ["username", username],
+                                        ["password", password], ["photoLink", photoLink], ["email", email]]) {
+        errorObj = await checkString(input, inputName);
+        if (errorObj.errorCode !== 0) {
+            return errorObj;
+        }
+    }
+
+    errorObj = await checkDateOfBirth(dateOfBirth, dateOfBirthObj);
+    if (errorObj.errorCode !== 0) {
+        return errorObj;
+    }
+
+    if (requestType === 'POST') {
+        if (await usersData.usernameExists(username)) {
+            errorCode = 400;
+            message = "Username already exists";
+            return {errorCode: errorCode, message: message};
+        }
+    }
+
+
+    errorObj = await checkAddress(address);
+    if (errorObj.errorCode !== 0) {
+        return errorObj;
+    }
+
+
+    // check photo link
+    if (!/^([a-z]:)?((\\|\/)[a-z0-9\s_@\-^!#$%&+={}\[\]]+)+\.(tif|tiff|bmp|jpg|jpeg|png|gif)$/i.test(photoLink)) {
+        errorCode = 400;
+        message = "'photoLink' parameter must be a valid full file path for an image of one the following types: .tif, .tiff, " +
+            ".bmp, 'jpg', 'jpeg', 'png', 'gif'.";
+        return {errorCode: errorCode, message: message};
+    }
+
+    if (requestType === 'POST') {
+        errorObj = await checkEmail(email);
+        if (errorObj.errorCode !== 0) {
+            return errorObj;
+        }
+    }
+
     return {errorCode: 0, message: ""};
 }
 
