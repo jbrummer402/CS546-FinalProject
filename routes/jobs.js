6313943034983f6108caa22fa875918e6e870d50
Jack Brummer <jbrummer@stevens.edu>
2021-05-13 18:44:29 -0400

    get job by id route

--- routes/jobs.js
+++ routes/jobs.js
@@ -56,6 +56,23 @@ router.get('/search/:searchTerm', async (req, res) => {
     console.log(e);
     res.status(404).json({ error: "job not found" });
   }
+});
+
+router.get('/:id', async(req, res) => {
+  if (!req.params.id) {
+    res.status(404).send({message: "Id is not given"});
+    return;
+  }
+  if (!ObjectId.isValid(xss(req.params.id))) {
+    res.status(404).send({message: "Id is not valid"});
+    return;
+  }
+  try {
+    const jobById = await jobsData.readByID(xss(req.params.id));
+    res.json(jobById);
+  } catch (e) {
+    res.status(500).send({error : e});
+  }
 })
 
 // to be implemented
