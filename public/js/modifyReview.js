54f1455f1ab9ef56a640ec3bbd3e1279e2e90f3f
Michael Arrabito <mikearrabito@gmail.com>
2021-05-14 19:55:27 -0400

    Update modifyReview.js

--- public/js/modifyReview.js
+++ public/js/modifyReview.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
 
         for(let review of reviewsList.children()){
             const reviewer = $(review).attr('reviewer');
-            const reviewid = $(review).attr('reviewid');
+            const reviewid = $(review).attr('id');
             if(reviewer == currentUserID){
                 $(review).append(`<button class="reviewEditButton" reviewid=${reviewid}>Edit Review</button>`);
                 $(review).append(`<button class="reviewDeleteButton" reviewid=${reviewid}>Delete Review</button>`);
